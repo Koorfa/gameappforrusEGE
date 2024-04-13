b8f946d97e4d5f5fcc82e3e562bb16c7378217b9
Алина <xilina.ugl@gmail.com>
2024-04-14 00:12:14 +0300

    разобралась с request.args

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 
 app = Flask(__name__)
 
@@ -7,6 +9,27 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('1.html')  # https://habr.com/ru/articles/804887/
+
+
+# https://www.digitalocean.com/community/tutorials/processing-incoming-request-data-in-flask-ru
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    return '''<h1>The language value is: {}</h1>'''.format(language)
+
+
+@app.route('/4')
+def task4():
+    mode = request.args.get('mode')  # question/answer
+    # if key doesn't exist, request.args.get() returns None
+    # while request.args[] returns error 400
+    word = request.args.get('word')  # id
+    answer = request.args.get('answer')  # right/wrong
+    if mode == 'answer':
+        return None
+    return render_template('1.html', word=word)
 
 
 if __name__ == '__main__':
